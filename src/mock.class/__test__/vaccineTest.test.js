d736b74285307380388621e46ffa1713e4ff82d5
Ke Liu <ke.liu1@thoughtworks.com>
2020-09-14 23:51:31 +0800

    [lk] finish vaccineTest.test

--- src/mock.class/__test__/vaccineTest.test.js
+++ src/mock.class/__test__/vaccineTest.test.js
@@ -4,24 +4,45 @@ import Covid19Vaccine from "../covid19Vaccine";
 
 jest.mock("../recipient", () => {
   // mock class实现
+  return jest.fn().mockImplementation(() =>
+  {
+    acceptInjection: mockAcceptInjection
+    getHasAntibodies: mockGetHasAntibodies
+  }
+
+  )
 });
 
 beforeEach(() => {
   // clear mock here
+  Recipient.mockClear();
+  mockAcceptInjection.mockClear();
 });
 
 describe("inject", () => {
   test("should recipient accept injection with vaccine", () => {
     // TODO 14: add test here
+    const vaccineTest = new vaccineTest();
+    vaccineTest.inject();
+    expect(acceptInjection).toHaveBeenCalledWith(expect.any(vaccine));
+
   });
 });
 
 describe("test", () => {
   test("should get Success if recipient has antibodies", () => {
     // TODO 15: add test here
+    let mockGetHasAntibodies = js.fn().mockImplementation(()=>true);
+    const result = vaccineTest.getHasAntibodies();
+    expect(getHasAntibodies).toHaveBeenCalled();
+    expect(result).toEqual("Vaccine Test Success")
   });
 
   test("should get Failed if recipient has no antibodies", () => {
     // TODO 16: add test here
+    let mockGetHasAntibodies = js.fn().mockImplementation(()=>false);
+    const result = vaccineTest.getHasAntibodies();
+    expect(getHasAntibodies).toHaveBeenCalled();
+    expect(result).toEqual("Vaccine Test Failed")
   });
 });
