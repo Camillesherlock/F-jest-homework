7bb3ec56279212721f035c0d4d9ab92145033757
Ke Liu <ke.liu1@thoughtworks.com>
2020-09-14 23:59:57 +0800

    [lk] finish user.test

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -4,11 +4,19 @@ jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async() => {
     // TODO 19: add test here
+    let username="fakeUserName";
+    let password="fakePassWord";
+    const result = register(username,password);
+    await expect(result).resolves.toEqual({})
   });
 
   test("should reject with Error when username is invalid", () => {
     // TODO 20: add test here
+    let username="fakeUserName";
+    let password="fakePassWord";
+    const result = register(username,password);
+    await expect(result).rejects.toEqual("wrong username or password")
   });
 });
