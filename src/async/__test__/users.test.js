e5d03407fd22538197e0c76f1401f710a201d4f6
Ke Liu <ke.liu1@thoughtworks.com>
2020-09-14 23:32:58 +0800

    [lk] add users.test

--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,5 +1,9 @@
+const { default: getUsers } = require("../users");
+
 describe("users", () => {
-  test("should get users data with mock axios get", () => {
+  test("should get users data with mock axios get", async() => {
     // TODO 13: add async test with manual mock
+    const result = getUsers();
+    await expect(result).resolves.toEqual({})
   });
 });
