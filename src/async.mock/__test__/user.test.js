bdc3d4c82c876dde4a55f9ca02f180ca26a312af
mengqiu PENG <mengqiu.peng@thoughtworks.com>
2020-09-24 22:59:49 +0800

    [mengqiu]  add feedback

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -4,16 +4,18 @@ import { verifyUsername } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
-let username="fakeUserName";
-let password="fakePassWord";
+const username = "fakeUserName";
+const password = "fakePassWord";
 describe("register", () => {
   test("should post user when validated", () => {
     // TODO 19: add test here
+    // TODO feedback: comment的代码就删掉哈
+
     // let username="fakeUserName";
     // let password="fakePassWord";
     // register(username,password);
     // expect(axios.post).toHaveBeenCalled();
-    register(username,password);
+    register(username, password);
 
     expect(axios.post).toHaveBeenCalled();
   });
@@ -21,8 +23,8 @@ describe("register", () => {
   test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
     verifyUsername.mockImplementation(() => false);
-    await expect(
-    register(username,password)).rejects.toThrow("wrong username or password")
+    await expect(register(username, password)).rejects.toThrow(
+      "wrong username or password"
+    );
   });
-  
 });
